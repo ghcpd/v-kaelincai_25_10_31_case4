29ad5e2fee20ef1f0404167ee440c5d57efae322
Kaelin Cai (Centific Technologies Inc) <v-kaelincai@microsoft.com>
2025-10-31 16:58:18 +0800

    delete

--- S40/Project_A_Faulty/setup_original.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-python -m venv .venv_faulty
-source .venv_faulty/bin/activate
-pip install --upgrade pip
-pip install -r requirements_original.txt
--- S40/Project_B_Optimized/setup_optimized.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-set -euo pipefail
-python -m venv .venv_optimized
-source .venv_optimized/bin/activate
-pip install --upgrade pip
-pip install -r requirements_optimized.txt
